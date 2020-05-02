c344bd59fcf6684e2973629a569777f94ece2062
andrejbugi <andrejbugi96@gmail.com>
2020-05-02 20:16:53 +0200

    new form in new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # get 'articles/index'
   get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: :new_article
+  get '/articles/:id', to: 'articles#show', as: :article
 
   root to: 'articles#index'
-  get '/articles/:id', to: 'articles#show', as: :article
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
