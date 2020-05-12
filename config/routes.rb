97297309c572795cbbe3e05bc1262d888c343afa
andrejbugi <andrejbugi96@gmail.com>
2020-05-12 21:01:59 +0200

    add new action: destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # get 'articles/index'
   get '/articles', to: 'articles#index'
   post '/articles', to: 'articles#create'
@@ -6,7 +7,8 @@ Rails.application.routes.draw do
   get '/articles/:id', to: 'articles#show', as: :article
   get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
   patch '/articles/:id', to: 'articles#update'
-  
+  delete '/articles/:id', to: 'articles#destroy'
+
 
   root to: 'articles#index'
 
