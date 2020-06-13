955c438352fabc28fd8bd339d7c89eaf90679d83
andrejbugi <andrejbugi96@gmail.com>
2020-06-13 18:20:59 +0200

    Add password attribute to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,19 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  has_secure_password
+
   before_save :email_to_downcase
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  validates :password, presence: true, length: { minimum: 6, }
 
   private
 
   def email_to_downcase
-    self.email = email.downcase 
+    self.email = email.downcase
   end
 end
