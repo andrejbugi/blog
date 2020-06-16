8514f3241b875899a18896220ffdf9b3e5ac4382
andrejbugi <andrejbugi96@gmail.com>
2020-06-16 18:16:18 +0200

    add new user action in users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :users
+
   root to: 'articles#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
