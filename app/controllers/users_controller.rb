bd505c87d7450730fc72b0641deee496559e6a98
andrejbugi <andrejbugi96@gmail.com>
2020-06-23 20:18:39 +0200

    Sessions implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   def new
+    logged_in_notice if logged_in?
+
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+
+
     if @user.save
+      log_in(@user)
       redirect_to @user
     else
       render :new
