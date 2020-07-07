d084e2f6582c1b27c03ba621dcb08d05593598f0
andrejbugi <andrejbugi96@gmail.com>
2020-07-07 18:06:35 +0200

    tests for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment do
-  
+  describe 'associations' do
+    it { is_expected.to belong_to(:article) }
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:body) }
+  end
 end
