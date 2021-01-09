7eda4d085102cc80d71e720e3f824c21fc3c6541
andrejbugi <andrejbugi96@gmail.com>
2021-01-09 20:24:54 +0100

    correct some typos

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,9 +4,9 @@ class Conversation < ApplicationRecord
 
   has_many :personal_messages, -> { order(created_at: :asc) }, dependent: :destroy
 
-  validates :author, uniqueness: {scope: :receiver}
+  validates :author, uniqueness: { scope: :receiver }
 
-  scope :participating, -> (user) do
+  scope :participating, ->(user) do
     where("(conversations.author_id = ? OR conversations.receiver_id = ?)", user.id, user.id)
   end
 
@@ -18,7 +18,7 @@ class Conversation < ApplicationRecord
     author == user || receiver == user
   end
 
-  scope :between, -> (sender_id, receiver_id) do
+  scope :between, ->(sender_id, receiver_id) do
     where(author_id: sender_id, receiver_id: receiver_id).or(where(author_id: receiver_id, receiver_id: sender_id)).limit(1)
   end
 end
